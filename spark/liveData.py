89de4123230ac5329bd24dc44c936e7581848037
Frederick Ernesto Borges Noronha <frederickborgesnoronha@gmail.com>
2019-12-11 23:20:57 +0100

    1000 videos

--- spark/liveData.py
+++ spark/liveData.py
@@ -1,11 +1,16 @@
 from YoutubeDataAPI import YoutubeDataAPI
 from datetime import datetime
+from readRegions import region_dict
 import socket
 import sys
 import requests
 import json
 import os
 import csv
+
+# diccionario con las abreviaturas y el nombre completo del pais
+countries = region_dict()
+MAX_ROWS = 3000
 
 def processVideos(apikey,regionCode):
     '''
@@ -25,6 +30,7 @@ def processVideos(apikey,regionCode):
     end = False
     while (not end):
         if youtubeData != None:
+            print(youtubeData)
             videos = youtubeData['items']
 
             for video in videos:
@@ -46,6 +52,16 @@ def processVideos(apikey,regionCode):
                     commentCount = convertFormat(video['statistics']['commentCount'])
                 except:
                     commentCount = 0
+
+                try:
+                    likes = convertFormat(video['statistics']['likeCount'])
+                except:
+                    likes = 0
+
+                try:
+                    dislikes = convertFormat(video['statistics']['likeCount'])
+                except:
+                    dislikes = 0
                 
                 # This dict was created to have a readable code.
                 infoDict = {
@@ -57,8 +73,8 @@ def processVideos(apikey,regionCode):
                     "channelSubscribers" : subscribers,
                     "tags" : tags,
                     "viewCount" : convertFormat(video['statistics']['viewCount']),
-                    "likeCount" : convertFormat(video['statistics']['likeCount']),
-                    "dislikeCount" : convertFormat(video['statistics']['dislikeCount']),
+                    "likeCount" : likes,
+                    "dislikeCount" : dislikes,
                     "favoriteCount" : convertFormat(video['statistics']['favoriteCount']),
                     "commentCount" : commentCount
                 }
@@ -119,42 +135,50 @@ def generateCSV(videos,regionCode):
     print("CSV file {} generated successfully.".format(file_name))
     pass
 
-
 if __name__ == "__main__":
     apikey = "AIzaSyDbJzfwtRtzYEn_CC9eNoxFn5JjF7qRx_U"
 
     ## ARGUMENT PARSER
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("regionCode", help="Region code for the youtube videos")
-    parser.add_argument("-m","--mode", help='live or csv, by default is csv')
+    helpRegionCode = 'Region code for the youtube videos, by default ES.\nPossible regions: ALL: for all regions\nCA: Canada,\n\tDE: Alemania,\n\tFR: Francia,\n\tGB: Reino Unido,\n\tIN: India,\n\tJP: Japon,\n\tKR: Korea,\n\tMX: Mexico,\n\tRU: Rusia,\n\tUS: Estados Unidos'
+    parser.add_argument("regionCode", help=helpRegionCode, default="ES")
+    parser.add_argument("-m","--mode", help='live or csv, by default is csv', default="CSV")
     args = parser.parse_args()
     ## END OF ARGUMENT PARSER
 
-    if (args.mode):
-        if (args.mode == "live"):
-            TCP_IP = "localhost"
-            TCP_PORT = 9009
-            conn = None
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.bind((TCP_IP, TCP_PORT))
-            s.listen(1)
-            print("Waiting for TCP connection...")
-            conn, addr = s.accept()
-            print("Connected... Starting getting tweets.")
-            resp = processVideos(apikey,args.regionCode)
-            sentVideosToSpark(resp,conn)
-        elif (args.mode == "csv"):
-            resp = processVideos(apikey,args.regionCode)
-            generateCSV(resp,args.regionCode)
+    if (args.mode.upper() == "LIVE"):
+        TCP_IP = "localhost"
+        TCP_PORT = 9009
+        conn = None
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(1)
+        print("Waiting for TCP connection...")
+        conn, addr = s.accept()
+        print("Connected... Starting getting tweets.")
+        if (args.regionCode.upper() == "ALL"):
+            resp = list()
+            for code in countries.keys():
+                resp += processVideos(apikey,code) 
+                print(len(resp))
+                if (len(resp) > MAX_ROWS): break
         else:
-            FAIL = '\033[91m'
-            ENDC = '\033[0m'
-            print('{0}"{1}" is not a valid mode.{2}'.format(FAIL,args.mode,ENDC))
-            sys.exit(1)
-    else:
-        resp = processVideos(apikey,args.regionCode)
+            resp = processVideos(apikey,args.regionCode.upper())
+        sentVideosToSpark(resp,conn)
+    elif (args.mode.upper() == "CSV"):
+        if (args.regionCode.upper() == "ALL"):
+            resp = list()
+            for code in countries.keys():
+                resp += processVideos(apikey,code)
+                print(len(resp))
+                if (len(resp) > MAX_ROWS): break
+        else:
+            resp = processVideos(apikey,args.regionCode.upper())
         generateCSV(resp,args.regionCode)
-
-    
+    else:
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        print('{0}"{1}" is not a valid mode.{2}'.format(FAIL,args.mode,ENDC))
+        sys.exit(1)
     pass
